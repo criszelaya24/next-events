a1d4ef10a544db37bd0e5cb9fddc85ba3b072fa2
cristopher Palacios <criszelaya24@hotmail.com>
2022-10-25 13:46:30 -0600

    Adding search form

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,19 @@
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/eventList';
+import { useRouter } from 'next/router';
+import EventsSearch from '../../components/events/events-search';
 const Events = () => {
+    const router = useRouter();
     const events = getAllEvents();
+    const findEventsHandler = (year, month) => {
+        const fullPath = `/events/${year}/${month}`;
+
+        router.push(fullPath);
+    };
 
     return (
         <>
+            <EventsSearch onSearch={findEventsHandler}/>
             <EventList events={events}/>
         </>
     );
--- ui/button.js
+++ ui/button.js
@@ -1,8 +1,18 @@
 import Link from 'next/link';
 
-const Button = ({ children, link }) => {
+const Button = ({ children, link, onClick }) => {
+    if (link) {
+        return (
+            <Link href={link}>
+                <a className='btn'>{children}</a>
+            </Link>
+        );
+    }
+
     return (
-        <Link href={link}><a className='btn'>{children}</a></Link>
+        <button className='btn' onClick={onClick}>
+            {children}
+        </button>
     );
 };
 
