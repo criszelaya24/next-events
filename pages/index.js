af2cf899a83d8b91434bf39db76487644c81825a
cristopher Palacios <criszelaya24@hotmail.com>
2022-10-25 15:56:50 -0600

    Using getStaticProps to simulate fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import { getFeaturedEvents } from '../dummy-data';
 import EventList from '../components/events/eventList';
 
-const Home = () => {
-    const events = getFeaturedEvents();
-
+const Home = ({ events }) => {
     return (
         <>
             <EventList events={events}/>
         </>
     );
 };
+
+export const getStaticProps = () => new Promise(res => {
+    return setTimeout(() => {
+        const events = getFeaturedEvents();
+
+        res({
+            props: { events },
+        });
+    }, 1000);
+});
 
 export default Home;
