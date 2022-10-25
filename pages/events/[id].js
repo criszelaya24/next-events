633b9a96d6866efe38b6d3c23a622eef5e86611e
cristopher Palacios <criszelaya24@hotmail.com>
2022-10-25 14:18:47 -0600

    Adding error message to single event

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -3,12 +3,13 @@ import EventContent from '../../components/event-detail/event-content';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventSummary from '../../components/event-detail/event-summary';
 import { getEventById } from '../../dummy-data';
+import ErrorAlert from '../../ui/error-alert';
 
 const Event = () => {
     const { query: { id } } = useRouter();
     const event = getEventById(id);
 
-    if (!event) return <>Error, event not found</>;
+    if (!event) return <ErrorAlert>event not found</ErrorAlert>;
 
     return (
         <>
