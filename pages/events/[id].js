0a7e59fd8b110edf3c4dc0a7c86604a1a9a75bad
cristopher Palacios <criszelaya24@hotmail.com>
2022-10-25 13:25:11 -0600

    Adding event Detail components

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,7 +1,22 @@
+import { useRouter } from 'next/router';
+import EventContent from '../../components/event-detail/event-content';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventSummary from '../../components/event-detail/event-summary';
+import { getEventById } from '../../dummy-data';
+
 const Event = () => {
+    const { query: { id } } = useRouter();
+    const event = getEventById(id);
+
+    if (!event) return <>Error, event not found</>;
+
     return (
         <>
-            <h1>Individual Event</h1>
+            <EventSummary event={event}/>
+            <EventLogistics event={event}/>
+            <EventContent>
+                { event.description }
+            </EventContent>
         </>
     );
 };
